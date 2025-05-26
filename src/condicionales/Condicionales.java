e7b20cf9163991cc25678850df1e8c3b786a0022
Hiram Méndez <hirammendez000@gmail.com>
2025-05-26 15:47:09 -0600

    condicionales

--- src/condicionales/Condicionales.java
+++ src/condicionales/Condicionales.java
@@ -13,5 +13,49 @@ public class Condicionales {
         } else {
             System.out.println("eres menor de edad");
         }
+
+        final byte day = 0;
+
+        switch (day) {
+            case 1:
+                System.out.println("lunes");
+                break;
+            case 2:
+                System.out.println("martes");
+                break;
+            case 3:
+                System.out.println("miercoles");
+                break;
+            case 4:
+                System.out.println("jueves");
+                break;
+            case 5:
+                System.out.println("viernes");
+                break;
+            case 6:
+                System.out.println("sabado");
+                break;
+            case 7:
+                System.out.println("domingo");
+                break;
+            default:
+                System.out.println("No valido!!");
+        }
+
+
+        // Esto nunca lo habia pensado xd, hay una forma muy chida
+        // de hacer que los condicionales sean mas facil de entender para otros desarrolladores...
+        final byte grade = 4;
+        final boolean isValid = grade >= 0 && grade <= 10;
+        final boolean isApproved = grade >= 6;
+
+        if ( isValid && isApproved ) {
+            System.out.println("Has pasado");
+        } else if (isValid) {
+            System.out.println("No has pasado el curso");
+        } else {
+            System.out.println("nota no valida");
+        }
+
     }
-}
+ }
