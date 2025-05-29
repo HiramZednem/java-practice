7b992ce43a43c318283f183149dff151e74adf0e
Hiram Méndez <hirammendez000@gmail.com>
2025-05-29 17:35:27 -0600

    encapsulation example

---
+++ src/poo/Main.java
@@ -0,0 +1,10 @@
+package poo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Vehicle ignis = new Vehicle("Susuki", "ignis", 2024);
+
+        ignis.setYear(2003);
+    }
+}
---
+++ src/poo/Vehicle.java
@@ -0,0 +1,52 @@
+package poo;
+
+public class Vehicle {
+    private String brand;
+    private String model;
+    private int year;
+
+    public Vehicle(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public void setYear(int year) {
+        final int currentYear = java.time.Year.now().getValue();
+        if ( year < 1886 || year > currentYear + 2) {
+            throw new IllegalArgumentException("Anio invalido");
+        }
+
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
